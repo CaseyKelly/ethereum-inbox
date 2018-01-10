ec9715008ba922cd3445c82011042078ce2ff57d
Casey Kelly <casey.kelly@colorado.edu>
2018-01-10 13:13:10 -0700

    pass in initial message from constant inside of Inbox contract test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,6 +9,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INTIAL_MESSAGE = 'Hi there!';
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -16,9 +17,9 @@ beforeEach(async () => {
 
   // use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode, arguments: [INTIAL_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' })
-  
+
   inbox.setProvider(provider);
 });
 
@@ -28,6 +29,6 @@ describe('Inbox', () => {
   });
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi there!');
+    assert.equal(message, INTIAL_MESSAGE);
   });
 });
